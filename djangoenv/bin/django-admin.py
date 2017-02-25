994b1deaff85c84fdca1fb705c94127b23bb2ee4
Samuel Armbrust <Samuel@MacBook-Pro-do-Samuel.local>
2017-02-25 00:30:17 -0300

    My Django project, first commit

---
+++ djangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/teste/Documents/current_developments/djangoApp/djangoenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
